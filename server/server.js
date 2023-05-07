dd2a8db17cde44f1cee40aaebd136ff08044fd86
vpolier <111066480+vpolier@users.noreply.github.com>
2023-05-08 05:29:29 +1000

    ignore node_modules

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 // setup apollo server and use typeDefs, resolvers, and auth for context
 const server = new ApolloServer({
